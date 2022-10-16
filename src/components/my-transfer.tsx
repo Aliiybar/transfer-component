dde02e3f4c7d112aee9552e261619804beaaf02e
Ali Iybar <ali.iybar@gmail.com>
2022-10-16 20:45:32 +0100

    multiselect

--- src/components/my-transfer.tsx
+++ src/components/my-transfer.tsx
@@ -6,6 +6,12 @@ export interface IArrayModel
    value: number;
    label: string;
 }
+
+interface InputChangeInterface {
+    target: HTMLInputElement;
+  }
+
+  
 export default function MyTransfer(props:any) {
     const listSize = props.size??10
     const dataSource : IArrayModel[] = props.dataSource as IArrayModel[] ;
@@ -19,19 +25,37 @@ export default function MyTransfer(props:any) {
         sourceDataSource = sourceDataSource.filter(f => f.value !== i )
     })
     
-    const [sourceSelected, setSourceSelected] = useState(-1);
-    const [targetSelected, setTargetSelected] = useState(-1);
+    const [sourceSelected, setSourceSelected] = useState<string[]>([]);
+    const [targetSelected, setTargetSelected] = useState<string[]>([]);
+
+    const handleSourceChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        sourceSelected.length > 0 && sourceSelected.includes(e.target.value) ? 
+            sourceSelected.length == 1 ? setSourceSelected([]) : 
+                setSourceSelected(sourceSelected.filter(k=> k!== e.target.value))
+        :
+            setSourceSelected([...sourceSelected, e.target.value])
+        ;
+    }
+
+    const handleTargetChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        targetSelected.length > 0 && targetSelected.includes(e.target.value) ? 
+            targetSelected.length == 1 ? setTargetSelected([]) : 
+                setTargetSelected(targetSelected.filter(k=> k!== e.target.value))
+        :
+            setTargetSelected([...targetSelected, e.target.value])
+        ;
+    }
 
     const sourceToTarget = () => {
         let newSelection = selectedDataKeys 
-        newSelection.push(sourceSelected)
-        setSourceSelected(-1)
+        sourceSelected.map(k=> newSelection.push(parseInt(k)))
+        setSourceSelected([])
         props.setSelectedDataKeys(newSelection)
     }
 
     const targetToSource = () => {
-        let newSelection = selectedDataKeys.filter(k=>k !== targetSelected)
-        setTargetSelected(-1)
+        let newSelection = selectedDataKeys.filter(k=> !targetSelected.includes(k.toString()))
+        setTargetSelected([])
         props.setSelectedDataKeys(newSelection)
     }
        
@@ -40,17 +64,28 @@ export default function MyTransfer(props:any) {
         <div className="transfer">
             <div className="transferSource">
                  <div>{props.sourceTitle}</div>
-                <select  size={listSize} onChange={(event) =>setSourceSelected(parseInt(event.target.value)) }>
+                <select  
+                    size={listSize}  
+                    multiple={true}
+                    onChange={handleSourceChange }
+                    value={sourceSelected}
+                >
                     {sourceDataSource.map(i=> <option value={i.value} key={i.value}>{i.label}</option>)}
                 </select>
             </div>
             <div className="transferButtons">
-                <button disabled={sourceSelected < 0} onClick={sourceToTarget}> &gt; </button>
-                <button disabled={targetSelected < 0} onClick={targetToSource}> &lt; </button>
+                <button disabled={sourceSelected.length < 0} onClick={sourceToTarget}> &gt; </button>
+                <button disabled={targetSelected.length < 0} onClick={targetToSource}> &lt; </button>
             </div>
             <div className="transferTarget">
                 <div>{props.targetTitle}</div>
-                <select size={listSize} onChange={(event) =>setTargetSelected(parseInt(event.target.value)) }>
+                <select 
+                    size={listSize} 
+                    multiple={true}
+                    onChange={handleTargetChange }
+                    value={targetSelected}
+                    
+                >
                     {targetDataSource.map(i=> <option value={i.value} key={i.value}>{i.label}</option>)}
                 </select>
             </div>
